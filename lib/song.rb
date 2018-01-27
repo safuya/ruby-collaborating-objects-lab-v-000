a8d1d5f75642bb2248fd7e5a5b82e27827a81c15
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-27 15:46:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    #binding.pry
     artist_name = filename.split(" - ")[0]
     song_name = filename.split(" - ")[1]
+    binding.pry
     artist = Artist.find_or_create_by_name(artist_name)
     song = new(song_name)
     artist.add_song(song)
