7453777ffbc1e892ea0126ae02b9c295dbef37ce
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-27 15:09:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,7 @@ class Song
     artist_name = filename.split(" - ")[0]
     song_name = filename.split(" - ")[1]
     artist = Artist.find_or_create_by_name(artist_name)
+    binding.pry
     song = new(song_name)
     artist.add_song(song)
     artist.save
